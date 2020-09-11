141f6437b7486c0b62d4fb78a80a8404698e904a
Joshua Stevenson <joshuas153@gmail.com>
2020-09-11 20:13:04 +1000

    add hover animations

--- NSCollectionView-Sample-Project/NSCollectionViewExample/CollectionViewItem.swift
+++ NSCollectionView-Sample-Project/NSCollectionViewExample/CollectionViewItem.swift
@@ -11,15 +11,49 @@ import Cocoa
 class CollectionViewItem: NSCollectionViewItem {
     
     @IBOutlet weak var numberLabel: NSTextField! // The label which will change depending on our data.
+    @IBOutlet weak var box: NSBox!
     var delegate: ItemDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let area = NSTrackingArea.init(rect: self.view.bounds, options: [NSTrackingArea.Options.mouseEnteredAndExited, NSTrackingArea.Options.activeAlways], owner: self, userInfo: nil)
+        self.view.addTrackingArea(area)
         // Do view setup here.
     }
     
     @IBAction func doSomething(_ sender: Any) {
         self.delegate?.buttonPressed(val: "I am " + numberLabel.stringValue)
+    }
+    
+    override func mouseEntered(with event: NSEvent) {
+        super.mouseEntered(with: event)
+        
+        let frame = self.view.layer?.frame;
+        self.view.layer?.anchorPoint = CGPoint(x: 0.5, y: 0.5);
+        self.view.layer?.frame = frame!; // Fix the location shift caused from anchor change
+        
+        let scaleAnimation:CABasicAnimation = CABasicAnimation(keyPath: "transform.scale")
+        scaleAnimation.duration = 0.2
+        scaleAnimation.fromValue = 1;
+        scaleAnimation.toValue = 1.04;
+        scaleAnimation.isRemovedOnCompletion = false;
+        scaleAnimation.fillMode = CAMediaTimingFillMode.forwards
+        self.view.layer?.add(scaleAnimation, forKey: "scale")
+    }
+    
+    override func mouseExited(with event: NSEvent) {
+        super.mouseExited(with: event)
+        
+        let frame = self.view.layer?.frame;
+        self.view.layer?.anchorPoint = CGPoint(x: 0.5, y: 0.5);
+        self.view.layer?.frame = frame!; // Fix the location shift caused from anchor change
+        
+        let scaleAnimation:CABasicAnimation = CABasicAnimation(keyPath: "transform.scale")
+        scaleAnimation.duration = 0.2
+        scaleAnimation.fromValue = 1.04;
+        scaleAnimation.toValue = 1;
+        scaleAnimation.isRemovedOnCompletion = false;
+        self.view.layer?.add(scaleAnimation, forKey: "scale")
     }
     
 }
